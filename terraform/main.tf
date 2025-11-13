82c73f4cefcacd93e8751243265c60140f2ec9d5
Nibin <nibinjohn29@gmail.com>
2025-11-13 12:54:37 +0530

    initial_12:54

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+#simple file resource
+
+resource "local_file" "freshfood_test" {
+  #filename = "terraform"
+  #filename = "${path.module}"
+  filename = "/Users/ASUS/OneDrive/Desktop/freshfoodtest-master/terraform/TFfile${count.index}.txt"
+  content = "Hi Nibin, Terraform says hi!!!!!!"
+  count = 3
+}
+
+resource "local_sensitive_file" "freshfood_test2" {
+  content  = "This is sensitive info"
+  filename = "${path.module}/TF2file.txt"
+}
