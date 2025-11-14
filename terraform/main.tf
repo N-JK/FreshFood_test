220264989a810655fe7cb9c272927b971d0847e8
Nibin <nibinjohn29@gmail.com>
2025-11-14 12:57:28 +0530

    practice

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
 #the file to mention the desired state
-
+locals {
+  environment = "dev"
+  upper_case = upper(local.environment)
+  basepath = "${path.module}/config/${local.upper_case}"
+}
 
 #simple file resource
 resource "local_file" "freshfood_test" {
   #filename = "terraform"
   #filename = "${path.module}"
-  filename = "/Users/ASUS/OneDrive/Desktop/freshfoodtest-master/terraform/TFfile${count.index}.txt"
+  filename = "/Users/ASUS/OneDrive/Desktop/freshfoodtest-master/terraform/${var.filename-1}.txt"
   content = "Hi Nibin, Terraform says hi!!!!!!"
-  count = 1
+  count = var.count_num
 }
 
 resource "local_file" "example1" {
-  filename = "${path.module}/example1.txt"
+  filename = "${path.module}/${var.filename-2}.txt"
   content = "Example for demo"
-  count = 1
+  count = var.count_num
 }
 
-resource "local_sensitive_file" "freshfood_test2" {
-  content  = "This is sensitive info"
-  filename = "${path.module}/TF2file.txt"
-}
---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "filename-1" {
+  description = "name of file 1"
+  default = "example"
+  type = string
+}
+variable "filename-2" {
+  description = "name of file 2"
+  default = "example2"
+  type = string
+}
+variable "filename-3" {
+  description = "name of file 3"
+  default = "example3"
+  type = string
+}
+
+variable "count_num" {
+  type = number
+  default = 1
+}
